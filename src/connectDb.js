9f8666b088144f05d44b5619abe24b7159248b49
Abhinav <abhinav@AVs-Mac.local>
2023-10-12 23:27:48 +0530

    [10 oct 2023] fix: err was not written in connectDb

--- src/connectDb.js
+++ src/connectDb.js
@@ -4,6 +4,6 @@ connect(process.env.DB_Connection_URL)
 .then(() => {
     console.log('connected to DB successfully');
 })
-.catch(() => {
+.catch((err) => {
     console.log(`Error: ${err.message}`);
 })
